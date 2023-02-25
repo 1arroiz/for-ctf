bfece519e17bc6c5f5e8c941af036b8157ffe4af
Евгений Копытин <33262744+1arroiz@users.noreply.github.com>
2023-02-25 19:18:59 +0300

    Update some.php

--- some.php
+++ some.php
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<!-- Contrbuted by GS -->
-<html><head></head><body>
-<?php
-while (list($var,$value) = each ($_ENV)) {
-    echo "$var => $value <br />";
-}
-?>
-</body></html>
+<?php echo phpinfo();?>
