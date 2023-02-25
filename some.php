221de16ce505cdcb38149d42fe4fd8f68705a2e3
Евгений Копытин <33262744+1arroiz@users.noreply.github.com>
2023-02-25 19:16:18 +0300

    Update some.php

--- some.php
+++ some.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<!-- Contrbuted by GS -->
+<html><head></head><body>
 <?php
-echo $_ENV('FLAG')
+while (list($var,$value) = each ($_ENV)) {
+    echo "$var => $value <br />";
+}
 ?>
+</body></html>
